cae2f4c07e0e155f9d982caeff49e6f82c9457e1
Jerónimo Cano <jeronimocano22@gmail.com>
2024-01-28 01:10:25 -0500

    working on waves

---
+++ Assets/1- Scripts/GameManager/WaveMovement.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class WaveMovement : MonoBehaviour
+{
+    //[SerializeField] private Transform innerShape, outerShape;
+    [SerializeField] private float cycleLength = 2;
+    [SerializeField] private float cycleLength2 = 1.7f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.DOMove(new Vector3(5, 0, 0), cycleLength).SetEase(Ease.Linear).SetLoops(-1, LoopType.Yoyo);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
